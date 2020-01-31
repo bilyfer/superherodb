eb727f55c3163f1978b8073af0b9a80b11ea6957
bchavez <bchavez@aclaratech.com>
2020-01-31 15:14:51 -0600

    agregando proyecto inicial de superherodb

---
+++ src/superherodb/Humano.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package superherodb;
+
+/**
+ *
+ * @author billy
+ */
+public class Humano extends Personaje implements Super {       
+    @Override
+    public void AgregarPoderes(String poder) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void AgregarDebilidades(String debilidad) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }    
+}
---
+++ src/superherodb/Personaje.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package superherodb;
+
+/**
+ *
+ * @author billy
+ */
+public abstract class Personaje {
+    private String nombre;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
